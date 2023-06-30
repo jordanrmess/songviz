3c9d0692b7397e7df4ea3bd910861345b1031642
Jordan Mess <jordanmess@Jordans-MacBook-Pro.local>
2023-06-30 01:31:35 -0400

    adding basic get calls for accuweather cities

--- app.js
+++ app.js
@@ -2,10 +2,52 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
+app.use(express.static('public'));
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+
+
+async function fetchData() {
+
+	const baseUrl = 'http://dataservice.accuweather.com/locations/v1/topcities/50'
+
+	const queryParameters = {
+		apikey: '6VH2JATPnnkmPu4oYQEkUcgDUdpWRG3S',
+		language: 'en-us',
+	};
+
+	var jsonResponse = {}
+
+
+	const queryString = new URLSearchParams(queryParameters).toString();
+	const apiUrl = `${baseUrl}?${queryString}`
+	var result
+	try {
+
+		const response = await fetch(apiUrl)
+			.then(response => response.json())
+			.then(data => {
+				return data
+			})
+			.catch(error => {
+				// Handle any errors that occurred during the API call
+				console.log(error);
+			})
+
+		
+		result=response
+
+	} catch (error) {
+		// Handle any errors that occurred during the API call
+		console.log(error);
+	}
+
+	return result
+
+}
+
+let cities = fetchData()
+cities.then(function(a) {
+	console.log(a)
+})
+
+
